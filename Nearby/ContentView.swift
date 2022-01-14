9c7530101b2b5b62dbe01dc720796f07a4cd6f73
Srinivasan Rajendran <srinivasan.rajendran@truecaller.comm>
2022-01-14 18:56:09 +0100

    Initial Commit

---
+++ Nearby/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Nearby
+//
+//  Created by Srinivasan Rajendran on 2022-01-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Nearby/NearbyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearbyApp.swift
+//  Nearby
+//
+//  Created by Srinivasan Rajendran on 2022-01-14.
+//
+
+import SwiftUI
+
+@main
+struct NearbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
