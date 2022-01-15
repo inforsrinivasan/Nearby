0b6a34bfe2f9aedef321ad50d0df567b537fa6e7
Srinivasan Rajendran <srinivasan.rajendran@truecaller.comm>
2022-01-15 14:43:35 +0100

    add a helper extension to use the primary brand color in type safe manner

---
+++ Nearby/Color+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  Color+Extension.swift
+//  Nearby
+//
+//  Created by Srinivasan Rajendran on 2022-01-15.
+//
+
+import SwiftUI
+
+extension Color {
+    static let brandPrimary = Color("brandPrimary")
+}
