ef78c2dce477fe9f41edc8331b5e82320874f67a
Srinivasan Rajendran <srinivasan.rajendran@truecaller.comm>
2022-01-15 21:24:52 +0100

    add skeleton and layout view for Locations List screen. extract AvatarView into its reusable view

--- Nearby/LocationsListView.swift
+++ Nearby/LocationsListView.swift
@@ -9,12 +9,52 @@ import SwiftUI
 
 struct LocationsListView: View {
     var body: some View {
-        Text("Locations List")
+        NavigationView {
+            List {
+                ForEach(0..<10) { _ in
+                    HStack {
+                        Image("default-square-asset")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 80)
+                            .clipShape(Circle())
+                            .padding(.vertical, 8)
+
+                        VStack(alignment: .leading) {
+                            Text("Location Namesdfsdfsdfsdf dsfsdf")
+                                .font(.title2)
+                                .fontWeight(.semibold)
+                                .lineLimit(1)
+                                .minimumScaleFactor(0.75)
+
+                            HStack {
+                                AvatarView()
+                                AvatarView()
+                                AvatarView()
+                                AvatarView()
+                            }
+                        }
+                        .padding(.leading)
+                    }
+                }
+            }
+            .navigationTitle("Spots")
+        }
     }
 }
 
 struct LocationsListView_Previews: PreviewProvider {
     static var previews: some View {
         LocationsListView()
+    }
+}
+
+struct AvatarView: View {
+    var body: some View {
+        Image("default-avatar")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 35, height: 35)
+            .clipShape(Circle())
     }
 }
